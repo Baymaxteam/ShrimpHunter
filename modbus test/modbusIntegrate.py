08d6c647f48089e85a7767be41327b49b51131ab
qwedsazxc78 <qwedsazxc78@gmail.com>
2016-11-19 15:43:53 +0800

    modbus py
    Control test ok

--- modbus test/modbusIntegrate.py
+++ modbus test/modbusIntegrate.py
@@ -1,11 +1,12 @@
-# FreqAddr 		2000H = 8192		
+# FreqAddr 		2000H = 8192
 # ControlAddr 	2001H = 8193
 
 # Stop 			000001 – 0x01
 # FWD direction	010010  - 0x12 - 18
 # REV direction	100010  - 0x22 - 34
 # Change			110010 - 0x32 - 50
-# minimalmodbus - https://minimalmodbus.readthedocs.io/en/master/installation.html
+# minimalmodbus -
+# https://minimalmodbus.readthedocs.io/en/master/installation.html
 
 #!/usr/bin/env python
 import minimalmodbus
@@ -29,19 +30,21 @@ CommendREV = -1
 # MotorFreq variable
 CommendMotorFreq = 1500
 
+
 def writeMotionFreq(DeviceID, value):
 	# avoid to large freq
 	if (value > 6000):
 		value = 6000
 	if (value < -6000):
-		value = -6000	
+		value = -6000
 
 	if (value > 0):
 		DeviceID.write_register(MotorFreqAddr, value, 0, 6)
 		writeMotionControl(DeviceID, CommendFWD)
 	else:
 		DeviceID.write_register(MotorFreqAddr, -value, 0, 6)
-		writeMotionControl(DeviceID, CommendFWD)
+		writeMotionControl(DeviceID, CommendREV)
+
 
 def writeMotionControl(DeviceID, value):
 	if(value == 1):
@@ -51,46 +54,48 @@ def writeMotionControl(DeviceID, value):
 	else:
 		DeviceID.write_register(MotorControlAddr, MotorSTOP, 0, 6)
 
+
 def StopMotor():
 	instrument1.write_register(MotorControlAddr, MotorSTOP, 0, 6)
-	sleep(0.1) # Time in seconds.
+	sleep(0.1)  # Time in seconds.
 	instrument2.write_register(MotorControlAddr, MotorSTOP, 0, 6)
-	sleep(0.1) # Time in seconds.
+	sleep(0.1)  # Time in seconds.
 	instrument3.write_register(MotorControlAddr, MotorSTOP, 0, 6)
-	sleep(0.1) # Time in seconds.
+	sleep(0.1)  # Time in seconds.
+
 
 def writeAllMotionFreq(value1, value2, value3):
 	writeMotionFreq(instrument1, value1)
-	sleep(0.1) # Time in seconds.
+	sleep(0.1)  # Time in seconds.
 	writeMotionFreq(instrument2, value2)
-	sleep(0.1) # Time in seconds.
+	sleep(0.1)  # Time in seconds.
 	writeMotionFreq(instrument3, value3)
-	sleep(0.1) # Time in seconds.
+	sleep(0.1)  # Time in seconds.
 
 
 ########### init Modbus/RTU functon ###############
-instrument1 = minimalmodbus.Instrument('COM8',1) # port name (in decimal)
+instrument1 = minimalmodbus.Instrument('COM8', 1)  # port name (in decimal)
 instrument1.serial.baudrate = 9600   # Baud
 instrument1.serial.bytesize = 8
-instrument1.serial.parity   = serial.PARITY_NONE
+instrument1.serial.parity = serial.PARITY_NONE
 instrument1.serial.stopbits = 2
-instrument1.serial.timeout  = 1   # seconds
+instrument1.serial.timeout = 1   # seconds
 instrument1.mode = minimalmodbus.MODE_RTU   # rtu or ascii mode
 
-instrument2 = minimalmodbus.Instrument('COM8',2) # port name (in decimal)
+instrument2 = minimalmodbus.Instrument('COM8', 2)  # port name (in decimal)
 instrument2.serial.baudrate = 9600   # Baud
 instrument2.serial.bytesize = 8
-instrument2.serial.parity   = serial.PARITY_NONE
+instrument2.serial.parity = serial.PARITY_NONE
 instrument2.serial.stopbits = 2
-instrument2.serial.timeout  = 1   # seconds
+instrument2.serial.timeout = 1   # seconds
 instrument2.mode = minimalmodbus.MODE_RTU   # rtu or ascii mode
 
-instrument3 = minimalmodbus.Instrument('COM8',3) # port name (in decimal)
+instrument3 = minimalmodbus.Instrument('COM8', 3)  # port name (in decimal)
 instrument3.serial.baudrate = 9600   # Baud
 instrument3.serial.bytesize = 8
-instrument3.serial.parity   = serial.PARITY_NONE
+instrument3.serial.parity = serial.PARITY_NONE
 instrument3.serial.stopbits = 2
-instrument3.serial.timeout  = 1   # seconds
+instrument3.serial.timeout = 1   # seconds
 instrument3.mode = minimalmodbus.MODE_RTU   # rtu or ascii mode
 
 ############### main function #############
@@ -108,12 +113,13 @@ except ValueError:
 
 if (MotorCommend[0] is '0' and MotorCommend[1] is '0' and MotorCommend[2] is '0'):
     print("Stop Motor")
-    StopMotor() # stop all motor
-	sleep(1) # Time in seconds.
+    StopMotor()
+    sleep(1)
 else:
     print(MotorCommend)
-    writeAllMotionFreq(int(MotorCommend[0],int(MotorCommend[1],int(MotorCommend[2]) # control motor
-	sleep(1) # Time in seconds.
+    writeAllMotionFreq(int(MotorCommend[0]), int(MotorCommend[1]), int(MotorCommend[2]))
+    sleep(1)
+
+print("End program")
 
 
-################### End #####################
